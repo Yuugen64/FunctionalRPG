326c2321d6b576c12151895bf6386e51a041e829
Yuugen64 <yuugen64@protonmail.com>
2021-04-03 21:37:57 -0500

    [002-0.0.2]:: Code Addition >> Added the ability to get players name and select a class. Will expand validation next time.

--- rpg.cpp
+++ rpg.cpp
@@ -3,11 +3,66 @@
 
 using namespace std;
 
+string getPlayerName()
+{
+    string playerName;
+    
+    cout << "Hello! What is your name?" << endl;
+    getline (cin, playerName);
+
+    return playerName;
+}
+
+int getPlayerClass()
+{
+    int playerClassValue;
+
+    cout << "And what class would you like to play? (please enter number)" << endl;
+    cout << "[ 1 - Warrior | 2 - Mage | 3 - Assassin ]" << endl;
+    cin >> playerClassValue;
+
+    return playerClassValue;
+}
+
+
 int main()
 {
+    string playerName = getPlayerName();
+    int playerClassValue = getPlayerClass();
+    string playerClassName;
+    int infoFlag = 0;
 
-
+    //Will probably need to have separate flags for this logic process, whiteboard it out.
+    while(infoFlag == 0)
+        {
+            if (infoFlag == 0)
+            {
+                cout << "So " << playerName << ", you've chosen to play a ";
+                switch (playerClassValue)
+                {
+                    case 1:
+                        playerClassName = "Warrior";
+                        cout << playerClassName << endl;
+                        break;
+                    case 2:
+                        playerClassName = "Mage";
+                        cout << playerClassName << endl;
+                        break;
+                    case 3:
+                        playerClassName = "Assassin";
+                        cout << playerClassName << endl;
+                        break;
+                    default:
+                        cout << "There was an error at validating class name." << endl;
+                }
+                cout << endl;
+                cout << "Is this information correct?" << endl;
+                cout << "[ 1 - YES | 0 - NO ]" << endl;
+                cin >> infoFlag;
+            }
+        }
 
 
     return 0;
 }
+
